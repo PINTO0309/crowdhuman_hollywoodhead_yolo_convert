70a22f96afa5bd680575aee1601e4cfc5e7539ad
pinto0309 <rmsdh122@yahoo.co.jp>
2022-08-04 18:56:42 +0900

    update

---
+++ 01_crowhuman2yolo/crowdhuman_dataset_download.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gdown --id 1WiJXxSPboz82tnqrrZF7HPWUJOq1Irgd -O data/raw/CrowdHuman_train01.zip
+gdown --id 1_8_HyfG9aauJ-jiBqjnruMUFr9DOuuqb -O data/raw/CrowdHuman_train02.zip
+gdown --id 1vwYob0T0SrmEKXxLU-gEqXBFJ9ymuVei -O data/raw/CrowdHuman_train03.zip
+gdown --id 1i8GnCBw4taUIbNyvwLaR4abELCZ2d-yl -O data/raw/CrowdHuman_val.zip
+gdown --id 1tLWJTgKf5ztGSXIt8q7yGaeK65noO3UH -O data/raw/annotation_train.odgt
+gdown --id 1l2PE9ZnZbpU6h0v2xuWC0lXI0gJiE6H0 -O data/raw/annotation_val.odgt
